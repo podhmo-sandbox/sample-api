896c0cab30bf4f8085c5a8283d8ad0fc2fe327c8
podhmo <ababjam61@gmail.com>
2022-07-06 00:34:46 +0900

    Repository test all (#13)
    * test: add getTodos test
    
    * refactor: fail in option, in helper
    
    * test: update test
    
    * test: add test for delete

--- model/repository/todo_repository_test.go
+++ model/repository/todo_repository_test.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -9,6 +10,34 @@ import (
 	"github.com/podhmo-sandbox/sample-api/model/entity"
 	rt "github.com/podhmo-sandbox/sample-api/model/repository/repositorytest"
 )
+
+func TestGetTodos(t *testing.T) {
+	ctx := context.Background()
+
+	todos := []entity.Todo{
+		{Id: 10, Title: "go to bed", Content: "should sleep"},
+		{Id: 11, Title: "go to toilet", Content: "should"},
+	}
+	db, teardown := rt.NewDB(ctx, t, rt.WithTodo(todos))
+	defer teardown()
+
+	rt.AssertRowsCount(t, db, "todo", 2 /* want*/) // todo: checking by defer
+
+	repo := &todoRepository{DB: db}
+	got, err := repo.GetTodos()
+	if err != nil {
+		t.Errorf("unexpected error: %+v", err)
+	}
+
+	// order by id desc
+	want := todos
+	sort.Slice(want, func(i, j int) bool { return want[i].Id > want[j].Id })
+
+	type ref struct{ XS []entity.Todo }
+	if diff := cmp.Diff(ref{want}, ref{got}); diff != "" {
+		t.Errorf("GetContext() mismatch (-want +got):\n%s", diff)
+	}
+}
 
 func TestInsertTodo(t *testing.T) {
 	ctx := context.Background()
@@ -35,4 +64,47 @@ func TestInsertTodo(t *testing.T) {
 		t.Errorf("GetContext() mismatch (-want +got):\n%s", diff)
 	}
 	rt.AssertRowsCount(t, db, "todo", 1 /* want*/)
+}
+
+func TestUpdateTodo(t *testing.T) {
+	ctx := context.Background()
+
+	id := 10
+	todos := []entity.Todo{
+		{Id: id, Title: "go to bed", Content: "should sleep"},
+	}
+	db, teardown := rt.NewDB(ctx, t, rt.WithTodo(todos))
+	defer teardown()
+	rt.AssertRowsCount(t, db, "todo", 1 /* want*/)
+
+	want := entity.Todo{Id: id, Title: "*", Content: "**"}
+	repo := &todoRepository{DB: db}
+	if err := repo.UpdateTodo(want); err != nil {
+		t.Errorf("unexpected error: %+v", err)
+	}
+
+	gots, err := repo.GetTodos()
+	if err != nil {
+		t.Errorf("unexpected error: %+v", err)
+	}
+	if diff := cmp.Diff(want, gots[0]); diff != "" {
+		t.Errorf("GetContext() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestDeleteTodo(t *testing.T) {
+	ctx := context.Background()
+	id := 10
+	todos := []entity.Todo{
+		{Id: id, Title: "go to bed", Content: "should sleep"},
+	}
+	db, teardown := rt.NewDB(ctx, t, rt.WithTodo(todos))
+	defer teardown()
+	rt.AssertRowsCount(t, db, "todo", 1 /* want*/)
+
+	repo := &todoRepository{DB: db}
+	if err := repo.DeleteTodo(todos[0].Id); err != nil {
+		t.Errorf("unexpected error: %+v", err)
+	}
+	rt.AssertRowsCount(t, db, "todo", 0 /* want*/)
 }
