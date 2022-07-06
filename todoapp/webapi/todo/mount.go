a8fc0234b69c5f47c2e17d37cc7d17b3cbd42406
podhmo <ababjam61+github@gmail.com>
2022-07-06 22:02:21 +0900

    fix: need interface in tests

--- todoapp/webapi/todo/mount.go
+++ todoapp/webapi/todo/mount.go
@@ -4,11 +4,12 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func Mount(r chi.Router, repo todoRepository) {
+func Mount(r chi.Router, repo todoRepository) chi.Router {
 	r.Route("/todos", func(r chi.Router) {
 		r.MethodFunc("GET", "/", GetTodos(repo))
 		r.MethodFunc("POST", "/", PostTodo(repo))
 		r.MethodFunc("PUT", "/{todoId}", PutTodo(repo))
 		r.MethodFunc("DELETE", "/{todoId}", DeleteTodo(repo))
 	})
+	return r
 }
